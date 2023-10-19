d3b6cd77ec970d170c1a22635a1669278336a1c9
Manuel Mariscal <victormanuelmariscal@icloud.com>
2023-10-18 20:13:18 -0600

    Adding temp visual codes

--- pyhtonScripts/plt_3_65.py
+++ pyhtonScripts/plt_3_65.py
@@ -7,15 +7,17 @@ data1 = {
 
 data2 = {
     "x" : [i for i in range(1, 10)],
-    "y" : [i * i for i in range(1,10)]
+    "y" : [i * 10 for i in range(1,10)]
 }
 
 plt.plot(
     data1["x"],
     data1["y"],
+    color = "r"
 )
 plt.scatter(
     data2["x"],
-    data2|["y"],
+    data2["y"],
+    marker = "*"
 )
 plt.show()
